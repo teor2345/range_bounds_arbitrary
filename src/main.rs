6768921f9e538886631657332967803e3626dfe7
teor <teor@riseup.net>
2020-07-11 10:30:44 +1000

    Failing RangeBounds usage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use proptest::prelude::*;
 
+use std::ops::Bound::*;
+//use std::ops::RangeBounds;
+
 #[derive(Debug)]
 struct U24(u32);
 
@@ -7,7 +10,7 @@ impl Arbitrary for U24 {
     type Parameters = ();
 
     fn arbitrary_with(_args: ()) -> Self::Strategy {
-        prop_oneof![(0..(1_u32 << 24)).prop_map(U24),].boxed()
+        prop_oneof![(Included(0), Excluded(1_u32 << 24)).prop_map(U24),].boxed()
     }
 
     type Strategy = BoxedStrategy<Self>;
